ddfcc9cb1fc208e7d7b0e11aa6ae9813ed46714c
JoseAntonioGuardiola <guardiola271999@gmail.com>
2021-02-20 09:48:33 +0100

    Test Cociente

---
+++ Test/entornosDesarrollo3/TestCociente.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package entornosDesarrollo3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Guardi
+ *
+ */
+class TestCociente {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
