235974c2c896f89e7ce7d631bf7097d4d9a2c026
Elena <iam.elenavm@gmail.com>
2021-02-20 09:33:31 +0100

    Test Resta

---
+++ Test/entornosDesarrollo3/TestResta.java
@@ -0,0 +1,14 @@
+package entornosDesarrollo3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestResta {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
