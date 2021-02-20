c6283e2f91d8b26036d907edd51a85ceba6803d6
MerySagra <mariajsm5@hotmail.com>
2021-02-20 09:41:37 +0100

    Test Producto

---
+++ Test/entornosDesarrollo3/TestProducto.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package entornosDesarrollo3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Mery
+ *
+ */
+class TestProducto {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
